9ad84f34c313670a4a1edd909e2ca6baf5fc70be
Sarai Montes de Oca Vargas <s.montesdeocavargas@mnemo.mx>
2022-04-15 16:38:00 -0500

    Objeto trending topic

---
+++ weekly_mission_2/exercises/exercise1/twitter/trending_topic.js
@@ -0,0 +1,9 @@
+const trending_topic = {
+    title:'Viernes Santo',
+    hashtag:'#viernesSanto',
+    content:'Hoy es viernes santo',
+    total_tweets: 100,
+    photo:''
+}
+
+console.log(`El trending topic es ${trending_topic.title}`)
