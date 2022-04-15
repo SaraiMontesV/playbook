ec08b4c1e9465fd21f0663345ae83e03db10a6ca
Sarai Montes de Oca Vargas <s.montesdeocavargas@mnemo.mx>
2022-04-15 16:44:48 -0500

    Objeto biography

---
+++ weekly_mission_2/exercises/exercise1/facebook/biography.js
@@ -0,0 +1,13 @@
+const biography = {
+   cover_page: 'portada.png',
+   profile: 'perfil.png',
+   details: 'Detalles',
+   friends: 'Amigos',
+   photos: 'Fotos',
+   post: 'Publicaciones',
+   information: 'Información',
+   music:'Música',
+   movies: 'Películas'
+}
+
+console.log(`Tu biografía contiene: \n -${biography.details} \n -${biography.friends} \n -${biography.photos}`)
